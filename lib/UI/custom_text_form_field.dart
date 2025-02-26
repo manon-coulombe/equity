504bf75857763c30b7f2ac9c9e271f0989421f35
Manon Coulombe <manon.coulombe@octo.com>
2025-02-26 16:56:20 +0100

    handle validation custom text input

--- lib/UI/custom_text_form_field.dart
+++ lib/UI/custom_text_form_field.dart
@@ -1,43 +1,59 @@
 import 'package:flutter/material.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomTextFormField extends FormField<String?> {
   final String label;
   final TextEditingController controller;
   final String errorMessage;
 
-  const CustomTextFormField({
+  CustomTextFormField({
     super.key,
     required this.label,
     required this.controller,
     required this.errorMessage,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(label, style: TextStyle(fontSize: 18)),
-        SizedBox(height: 8),
-        TextFormField(
-          onTapOutside: (_) {
-            print('onTapOutside');
-            FocusManager.instance.primaryFocus?.unfocus();
-          },
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return errorMessage;
-            }
-            return null;
-          },
-          controller: controller,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-            fillColor: Colors.white,
-            filled: true,
-          ),
-        ),
-      ],
-    );
-  }
+  }) : super(validator: (_) {
+          if (controller.text.isEmpty) {
+            return errorMessage;
+          }
+          return null;
+        }, builder: (FormFieldState<String?> state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(label, style: TextStyle(fontSize: 18)),
+              SizedBox(height: 8),
+              TextFormField(
+                onTapOutside: (_) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+                controller: controller,
+                onChanged: (_) {
+                  if (state.hasError) {
+                    state.validate();
+                  }
+                },
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(
+                      color: state.hasError ? Colors.red : Color(0xFF000000),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(
+                      color: state.hasError ? Colors.red : Color(0xFF000000),
+                    ),
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+              if (state.hasError && state.errorText != null)
+                Text(
+                  state.errorText!,
+                  style: TextStyle(color: Colors.red),
+                ),
+            ],
+          );
+        });
 }
